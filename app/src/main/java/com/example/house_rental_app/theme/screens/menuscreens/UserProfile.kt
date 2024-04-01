1560bb7729f8962be8d4007be86ac19a17ab09c2
shivapriyasunnam <priya@rebl.ai>
2024-04-01 13:47:12 -0300

    user profile edit screen updated

--- app/src/main/java/com/example/house_rental_app/theme/screens/menuscreens/UserProfile.kt
+++ app/src/main/java/com/example/house_rental_app/theme/screens/menuscreens/UserProfile.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,11 +35,12 @@ import com.example.house_rental_app.data.SharedViewModel
 import com.example.house_rental_app.entity.UserEntity
 import com.example.house_rental_app.data.UserViewModel
 import com.example.house_rental_app.models.User
-
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun UserProfile(navController: NavController, sharedViewModel: SharedViewModel) {
+    val coroutineScope = rememberCoroutineScope()
     var editing by remember { mutableStateOf(false) }
     val userId = sharedViewModel.userId.observeAsState().value.toString().toInt()
 
@@ -49,123 +52,105 @@ fun UserProfile(navController: NavController, sharedViewModel: SharedViewModel) 
     }
     val userDetails by userViewModel.userDetails.observeAsState()
     userDetails?.let { user ->
-        var editedUser by remember { mutableStateOf(user) }
-    Column() {
+        var editedUser by remember { mutableStateOf(user) } // Remember the edited user state
+        Column() {
+            Spacer(modifier = Modifier.height(30.dp))
 
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(horizontal = 15.dp)
+            ) {
+                Text(text = "User Profile", style = MaterialTheme.typography.bodyLarge)
+                Spacer(modifier = Modifier.height(16.dp))
 
-//        val currentRoute = getCurrentRoute(navController) ?: ""
-//        Log.println(Log.INFO, "current route", currentRoute)
-//        MenuBar(navController = navController, currentRoute = currentRoute)
+                UserDetail("Email", if (editing) editedUser.emailId else user.emailId,
+                    onValueChange = { newValue ->
+                        editedUser = editedUser.copy(emailId = newValue) // Update edited user
+                    },
+                    isEditable = editing
+                )
 
-        // Spacer to add some space between menu bar and list
-        Spacer(modifier = Modifier.height(30.dp))
+                UserDetail("Password", if (editing) editedUser.password else "*****",
+                    onValueChange = { newValue ->
+                        editedUser = editedUser.copy(password = newValue) // Update edited user
+                    },
+                    isEditable = false // Password should not be editable
+                )
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(horizontal = 15.dp)
-        ) {
-            // Menu bar
+                UserDetail("Phone Number", if (editing) editedUser.phoneNumber else user.phoneNumber,
+                    onValueChange = { newValue ->
+                        editedUser = editedUser.copy(phoneNumber = newValue) // Update edited user
+                    },
+                    isEditable = editing
+                )
 
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Text(text = "User Profile", style = MaterialTheme.typography.bodyLarge)
-            Spacer(modifier = Modifier.height(16.dp))
-
-            UserDetail("Email", if (editing) editedUser.emailId else user.emailId) { newValue ->
-                editedUser.emailId = newValue
-            }
-
-            UserDetail("Password", if (editing) editedUser.password else "*****") { newValue ->
-                editedUser.password = newValue
-            }
-
-            UserDetail("Phone Number", if(editing) editedUser.phoneNumber else user.phoneNumber) { newValue ->
-                editedUser.phoneNumber = newValue
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            if (editing) {
-                TextButton(onClick = {
-                    userViewModel.updateUser(editedUser)
-//                    onUserUpdated(editedUser)
-                    editing = false
-                }) {
-                    Text(text = "Save")
-                }
-            } else {
-                TextButton(
-                    onClick = { run { editing = true } },
+                if (editing) {
+                    Button(onClick = {
+                        coroutineScope.launch { userViewModel.updateUser(editedUser)}
+                        editing = false
+                    }) {
+                        Text(text = "Save")
+                    }
+                } else {
+                Button(
+                    onClick = { editing = true },
                     modifier = Modifier
                         .border(
-                            width = 1.dp, // Border width
-                            color = Color.Black, // Border color
-                            shape = RoundedCornerShape(4.dp) // Border shape
-
+                            width = 1.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(4.dp)
                         )
                         .background(Color.LightGray)
-                        .padding(horizontal = 19.dp, vertical = 2.dp) // Padding for the button
+                        .padding(horizontal = 19.dp, vertical = 2.dp),
+                    // Add any additional styling here if needed
                 ) {
                     Text(
                         text = "Edit",
-                        style = TextStyle(color = Color.Black) // Text color
+                        style = TextStyle(color = Color.Black)
                     )
                 }
             }
+            }
         }
-    }
-    }?:run{
+    } ?: run {
         Text(text = "User not found or loading")
     }
 }
 
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun UserDetail(label: String, value: String, onValueChange: (String) -> Unit) {
-    val context = LocalContext.current
-
+private fun UserDetail(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    isEditable: Boolean
+) {
     Column(
         modifier = Modifier.padding(vertical = 8.dp)
     ) {
-        Text(text = "$label: ", style = MaterialTheme.typography.bodySmall)
-        if ((value != "*****") && label != "UserID") {
+        Text(text = "$label: ", style = MaterialTheme.typography.bodyLarge, modifier = Modifier.padding(horizontal = 100.dp).align(Alignment.CenterHorizontally))
+        if (isEditable) {
             TextField(
                 value = value,
-                onValueChange = { onValueChange(it) },
+                onValueChange = onValueChange,
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
                 modifier = Modifier.fillMaxWidth()
             )
         } else {
-
-            if(label== "Phone Number"){
-                TextField(
-                    value = value,
-                    onValueChange = { onValueChange(it) },
-                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
-            else {
-
-                Text(
-                    text = "*****",
-                    style = MaterialTheme.typography.headlineLarge,
-                    modifier = Modifier.fillMaxWidth(),
-                    maxLines = 1
-                )
-            }
-//            Text(text = "*****", style = MaterialTheme.typography.bodySmall)
+            Text(text = value, style = MaterialTheme.typography.bodyLarge)
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
 @Composable
 fun UserProfilePreview() {
     val navController = rememberNavController()
-    val user = UserEntity(emailId = "example@example.com", password =  "password123", id = 12, username = "Jane Doe", phoneNumber = "23", showOnlyEmail = true, showOnlyPhone = false )
-
-    UserProfile(navController = navController, sharedViewModel = SharedViewModel())
+    val sharedViewModel = SharedViewModel().apply { setUserId("8") }
+    UserProfile(navController = navController, sharedViewModel = sharedViewModel)
 }
